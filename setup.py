4a8edf2f8f229a0d00df3e5d37ae0dd6f3b26c3d
Robert C. Alfredson <robert.c.alfredson@gmail.com>
2024-12-19 15:33:04 -0500

    Update setup.py to distinguish this version of the code as a distinct fork.

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="contools",
-    version="2.0.1",
-    description="tools for analyzing connectomics data from CATMAID instances",
-    url="http://github.com/mwinding/connectome_tools",
-    author="Michael Winding",
-    author_email="mwinding@alumni.nd.edu",
+    name="contools-optimized",  # Updated name
+    version="2.0.2",
+    description="An optimized version of connectome_tools with performance enhancements and multi-processing",
+    url="http://github.com/rcalfredson/connectome_tools",
+    author="Michael Winding, updated by R.C. Alfredson",
+    author_email="mwinding@alumni.nd.edu, robert.c.alfredson@gmail.com",
     license="MIT",
     packages=find_packages(include=["contools", "contools.*"]),
     install_requires=["anytree", "python-catmaid", "scipy", "tables", "upsetplot"],
+    long_description=(
+        "This is an optimized fork of connectome_tools, originally authored by Michael Winding. "
+        "This version includes performance enhancements such as better memory management and multi-processing capabilities, "
+        "maintained by R.C. Alfredson. For the original repository, see http://github.com/mwinding/connectome_tools."
+    ),
 )
